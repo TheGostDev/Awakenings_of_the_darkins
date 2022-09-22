18450a0aa2037f87b3ed98b33e5b9cce8bf154cf
THS-TheGost <romain.boisseau.18@gmail.com>
2022-09-23 01:18:19 +0200

    Adding BLOCKS "method"

--- src/main/java/com/darkins/awakenings_of_the_darkins/block/ModBlocks.java
+++ src/main/java/com/darkins/awakenings_of_the_darkins/block/ModBlocks.java
@@ -1,8 +1,13 @@
 package com.darkins.awakenings_of_the_darkins.block;
 
+import com.darkins.awakenings_of_the_darkins.Awakenings_Of_The_Darkins;
 import net.minecraft.world.level.block.Block;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
 public class ModBlocks {
 
+    public static final DeferredRegister<Block> BLOCKS =
+            DeferredRegister.create(ForgeRegistries.BLOCKS, Awakenings_Of_The_Darkins.MOD_ID);
 }
