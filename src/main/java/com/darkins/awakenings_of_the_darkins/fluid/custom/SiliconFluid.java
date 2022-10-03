1bb8103ff388f48dfde4ffcd460aa763cff74e6c
THS-TheGost <romain.boisseau.18@gmail.com>
2022-10-03 22:52:58 +0200

    Adding Silicon fluid properties

--- src/main/java/com/darkins/awakenings_of_the_darkins/fluid/custom/SiliconFluid.java
+++ src/main/java/com/darkins/awakenings_of_the_darkins/fluid/custom/SiliconFluid.java
@@ -1,39 +1,31 @@
 package com.darkins.awakenings_of_the_darkins.fluid.custom;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.LiquidBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.FlowingFluid;
 
-import java.util.*;
 import java.util.function.Supplier;
 
 public class SiliconFluid extends LiquidBlock {
+
 
     public SiliconFluid(Supplier<? extends FlowingFluid> pFluid, Properties pProperties) {
         super(pFluid, pProperties);
     }
 
+    public void entityInside(BlockState pState, Level pLevel, BlockPos pPos, Entity pEntity) {
+            if (!pLevel.isClientSide && (pEntity.xOld != pEntity.getX() || pEntity.zOld != pEntity.getZ())) {
+                double d0 = Math.abs(pEntity.getX() - pEntity.xOld);
+                double d1 = Math.abs(pEntity.getZ() - pEntity.zOld);
+                if (d0 >= (double)0.003F || d1 >= (double)0.003F) {
+                    pEntity.hurt(DamageSource.LAVA, 3.0F);
+                }
+            }
 
-    /*
-
-    Ajouter l'effet Silicium - Eau
-    Modif Effet Poison VoxelShape -> Silicium (Render Block =/ Liquid texture)
-
-     */
-
-    @Override
-    public void stepOn(Level level, BlockPos pos, BlockState state, Entity entity) {
-        if(entity instanceof LivingEntity livingEntity) {
-            livingEntity.addEffect(new MobEffectInstance(MobEffects.POISON, 100));
-        }
-
-        super.stepOn(level, pos, state, entity);
     }
 
 }
